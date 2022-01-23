c462ff5853b2212272a9490158b6421c33cd37bd
Gabriel Soares <gsdvl2004@gmail.com>
2022-01-23 05:44:45 -0300

    Inventario base
    Os comentários do código dizem o que fazer, Mas em geral, no momento o principal é fazer com que a lista comece vazia, e ir preenchendo ela com os itens do script ItemList, enquanto seta os boleanos de "hasItem"

---
+++ Game/Assets/Scripts/ItemList.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemList : MonoBehaviour
+{
+    //Código que irá conter uma lista com todos os itens, e seu ID
+     public Sprite[] Itens;
+
+}
